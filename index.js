2333b04f3239c038a1a2a0d9b0c6f7ea55d5dfd3
ApexcupDeveloper <apexcup199096@gmail.com>
2023-11-16 17:34:36 +0800

    add node runner

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(3005, () => {
+    console.log('Server successfully running on port 3005');
+});
